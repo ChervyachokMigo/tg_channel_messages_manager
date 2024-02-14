0e980d926ea9b72bff6c97d4b3637f035a2f3627
tester_pivka17 <github@svdgod.ru>
2024-02-14 08:11:13 +0500

    Update extract_oszs.js

--- tools/extract_oszs.js
+++ tools/extract_oszs.js
@@ -4,40 +4,41 @@ const path = require('path');
 const { folder_prepare } = require('../misc/tools');
 const { spawnSync } = require('child_process');
 
-const exe = path.join('bin', '7z.exe');
-const extract_base_path = path.join(download_folder, extract_folder);
-folder_prepare(extract_base_path);
+const exe = path.join('..', 'bin', '7z.exe');
 
 // eslint-disable-next-line no-undef
 const work_path = path.dirname( process.argv.slice(1).shift() );
+const extract_relative_path = path.join(download_folder, extract_folder);
+const download_path = path.join(work_path, download_folder);
+
+folder_prepare(extract_relative_path);
+
 
 module.exports = () => {
-
     const downloaded_files = fs.readdirSync( download_folder, 'utf8' );
-
     if (downloaded_files.length > 0) {
 
         console.log('extracting downloaded files');
-
         for (let filename of downloaded_files){
+
             if (path.extname(filename) === '.osz'){
-                const archieve_path = path.join( work_path, download_folder, filename);
-                const extract_path = path.join(work_path, extract_base_path, path.basename(filename, '.osz'));
-                if (fs.existsSync(extract_path)){
+                const extract_archieve_path = path.join(extract_relative_path, path.basename(filename, '.osz'));
+                if (fs.existsSync(extract_archieve_path)){
                     console.log('file already extracted, skip', filename);
                     continue;
+
                 } else {
-                    console.log('extracting', archieve_path, 'to', extract_path);
-                    //folder_prepare(extract_path);
+                    console.log('extracting', filename, 'to', extract_archieve_path);
                     const args = [ 
                         'x',    //extract files
-                        '-y',   //assume yes
-                        archieve_path,
-                        extract_path + path.delimiter,
-                        '*'
+                        '-bd',
+                        filename,
+                        `-o${path.join(extract_folder, path.basename(filename, '.osz'))}`,
                     ];
-                    const {stdout, stderr} = spawnSync(exe, args, {encoding: 'utf8', cwd: extract_path});
-                    console.log(stdout, stderr);
+                    const { stderr } = spawnSync(exe, args, { encoding: 'utf8', cwd: download_path });
+                    if (stderr) {
+                        console.log(stderr)
+                    }
                 }
             }
         }
