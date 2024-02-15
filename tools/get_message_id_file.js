26de51ddb83fb92b9ae94af2f1934765550844df
tester_pivka17 <github@svdgod.ru>
2024-02-15 08:55:13 +0500

    Update get_message_id_file.js

--- tools/get_message_id_file.js
+++ tools/get_message_id_file.js
@@ -3,10 +3,10 @@ module.exports = (media_1, media_2) => {
         return { error: 'no files' };
 
     if (!media_1.media_type)
-        return media_1.id;
+        return media_1.id ?? null;
 
     if (media_2 && !media_2.media_type)
-        return media_2.id;
+        return media_2.id ?? null;
 
     return { error: 'no osz' }
 };
